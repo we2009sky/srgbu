22742bc0ebc22a62c553ccb2c8c39b3aa03ccb84
danielli <daniel.mail.com>
2018-12-07 21:26:30 +0800

    commit viper code to git

---
+++ viper/viper_bst.py
@@ -0,0 +1,26 @@
+
+
+class Act2(object):
+    """Program ACT2 for all project"""
+
+
+    def __init__(self):
+        pass
+
+
+
+    def manufacture_login(self):
+        pass
+
+
+    def install_clip(self):
+        pass
+
+    
+
+    def install_sudi(self):
+        pass
+
+
+    def close_login(self):
+        pass
